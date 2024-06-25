85b836de48f8c4817f5e58fa1cbbdd93b3fade9c
bdmoriki <dupim1@hotmail.com>
2024-06-24 21:59:16 -0300

    Adicionando aula 11 - file server

---
+++ 11_file_server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./11_file_server/public"))
+	mux := http.NewServeMux()
+	mux.Handle("/", fileServer)
+	mux.HandleFunc("/blog", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
