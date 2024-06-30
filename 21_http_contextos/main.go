fb2be8aacc25038abd7f124bb757b7cef62a6400
bdmoriki <dupim1@hotmail.com>
2024-06-30 19:56:30 -0300

    Adicionando aula 21 - Contexto Http

---
+++ 21_http_contextos/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	//ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "http://google.com", nil)
+	if err != nil {
+		panic(err)
+	}
+	client := http.Client{}
+	client.Do(req)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
+}
