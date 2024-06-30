27e9af3230f8aea392de599071859096999e2670
bdmoriki <dupim1@hotmail.com>
2024-06-30 15:06:49 -0300

    Adicionando aula de composicao de templates

---
+++ 16_compondo_templates/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"os"
+	"text/template"
+)
+
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+
+	templates := []string{
+		"header.html", "content.html", "footer.html"}
+
+	t := template.Must(template.New("content.html").ParseFiles(templates...))
+	err := t.Execute(os.Stdout, Cursos{
+		{Nome: "Go", CargaHoraria: 40},
+		{Nome: "Java", CargaHoraria: 80},
+		{Nome: "Python", CargaHoraria: 120},
+	})
+	if err != nil {
+		panic(err)
+	}
+}
